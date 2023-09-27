96d3062beee63ca2756958c879352dec5b96a61f
Cris Mateias <cristinel.mateias@mypulse.ai>
2023-09-27 12:39:01 +0100

    Add subscriptions api

---
+++ src/zebra/subscriptions.clj
@@ -0,0 +1,43 @@
+(ns zebra.subscriptions
+  (:refer-clojure :exclude [list])
+  (:require
+    [zebra.utils :refer [transform-params]])
+  (:import
+    (com.stripe.model
+      Subscription
+      StripeCollection)
+    (com.stripe.net
+      RequestOptions)
+    (java.util
+      Map)))
+
+(defn subscription->map
+  [^Subscription subscription]
+  {:id                   (.getId subscription)
+   :customer             (.getCustomer subscription)
+   :cancelled-at         (.getCancelAt subscription)
+   :created              (.getCreated subscription)
+   :current-period-start (.getCurrentPeriodStart subscription)
+   :current-period-end   (.getCurrentPeriodEnd subscription)
+   :status               (.getStatus subscription)
+   :metadata             (.getMetadata subscription)})
+
+(defn subscriptions->map
+  [^StripeCollection coll]
+  {:has-more      (.getHasMore coll)
+   :subscriptions (map subscription->map
+                       (.getData coll))})
+
+(defn create
+  [params api-key]
+  (subscription->map
+    (Subscription/create ^Map (transform-params params)
+                         (-> (RequestOptions/builder) (.setApiKey api-key) .build))))
+
+(defn list
+  ([params api-key]
+   (subscriptions->map
+     (Subscription/list ^Map (transform-params params)
+                        (-> (RequestOptions/builder) (.setApiKey api-key) .build))))
+  ([api-key]
+   (list {} api-key)))
---
+++ test/zebra/subscriptions_test.clj
@@ -0,0 +1,56 @@
+(ns zebra.subscriptions-test
+  (:require
+    [clojure.test :refer :all]
+    [zebra.customers :as customers]
+    [zebra.payment-methods :as payment-methods]
+    [zebra.prices :as prices]
+    [zebra.products :as products]
+    [zebra.subscriptions :as subscriptions]
+    [zebra.helpers.constants :refer [api-key]]))
+
+(deftest create-subscription
+  (let [customer (customers/create api-key)
+        customer-id (:id customer)
+        payment-method (payment-methods/create {:type "card"
+                                                :card {:number    "4242424242424242"
+                                                       :exp_month "7"
+                                                       :exp_year  "2026"
+                                                       :cvc       "314"}} api-key)
+        _attached-payment-method (customers/attach-payment-method customer-id (:id payment-method) api-key)
+        product (products/create {:name (str "test_product_" (random-uuid))} api-key)
+        product-id (:id product)
+        price (prices/create {:unit_amount 999
+                              :currency    "gbp"
+                              :recurring   {:interval "day"}
+                              :product     product-id} api-key)
+        price-id (:id price)
+        subscription (subscriptions/create {:customer               customer-id
+                                            :default_payment_method (:id payment-method)
+                                            :items                  [{:price price-id}]} api-key)]
+    (testing "should be a valid subscription"
+      (is (some? (:id subscription))))))
+
+(deftest list-subscriptions
+  (let [customer (customers/create api-key)
+        customer-id (:id customer)
+        payment-method (payment-methods/create {:type "card"
+                                                :card {:number    "4242424242424242"
+                                                       :exp_month "7"
+                                                       :exp_year  "2026"
+                                                       :cvc       "314"}} api-key)
+        _attached-payment-method (customers/attach-payment-method customer-id (:id payment-method) api-key)
+        product (products/create {:name (str "test_product_" (random-uuid))} api-key)
+        product-id (:id product)
+        price (prices/create {:unit_amount 999
+                              :currency    "gbp"
+                              :recurring   {:interval "day"}
+                              :product     product-id} api-key)
+        price-id (:id price)
+        subscription (subscriptions/create {:customer               customer-id
+                                            :default_payment_method (:id payment-method)
+                                            :items                  [{:price price-id}]} api-key)
+        retrieved-subscriptions (subscriptions/list api-key)]
+    (testing "should retrieve a list of subscriptions"
+      (is (some? retrieved-subscriptions))
+      (is (some? (:subscriptions retrieved-subscriptions)))
+      (is (some #(= (:id subscription) (:id %)) (:subscriptions retrieved-subscriptions))))))
